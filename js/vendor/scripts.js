25927f19ec8367fb7b24f70358103645667b7110
Fábián Gábor <dirtyred.photo@gmail.com>
2014-07-30 02:57:35 +0200

    render speed optimization

---
+++ js/vendor/scripts.js
@@ -0,0 +1,77 @@
+function scrollbarWidth() {
+	//var div = $('<div style="width:50px;height:50px;overflow:hidden;position:absolute;top:-200px;left:-200px;"><div style="height:100px;"></div>');
+	var div = $('<div style="width:50px;height:50px;overflow:hidden;position:absolute;translate3d(-200px, -200px, 0);"><div style="height:100px;"></div>');
+	// Append our div, do our calculation and then remove it
+	$('body').append(div);
+	var w1 = $('div', div).innerWidth();
+	div.css('overflow-y', 'scroll');
+	var w2 = $('div', div).innerWidth();
+	$(div).remove();
+	return (w1 - w2);
+}
+
+function fixHeights() {
+	var windowHeight = $( window ).height();
+	$( ".left-sidenav" ).height( windowHeight );
+	$( ".left-off-canvas-menu" ).height( windowHeight );	
+	$( ".single .placeholder" ).height( windowHeight );
+	document.getElementById('leftmenuIcon').style.transform = "translate3d(0, " + windowHeight/2 + "px, 0)";
+	document.getElementById('rightmenuIcon').style.transform = "translate3d(0, " + windowHeight/2 + "px, 0)";
+}
+
+function fixScrollbar() {
+	var scale = $(window).width() / ( $(window).width()-scrollbarWidth() );
+	$('.archive .main-section').css( 'margin-right', -scrollbarWidth() );
+	$('.single .main-section').css( 'margin-right', -scrollbarWidth() );
+	
+	//$('.archive .main-section').css('transform', 'scaleX(' + scale + ')' ); // distorting!
+}
+
+function cover() {
+	var windowHeight = $( window ).height();
+	//$("body.single-format-image #cover").height( windowHeight );	
+	var video = document.getElementById('video');
+	$("#video").css('transform', 'translate3d(0, ' + (windowHeight - $("#video").outerHeight())/2 + 'px, 0)' );
+	
+	//console.log( (windowHeight - $("#video").height())/2 );	
+}
+
+/*
+$("#scrollDown").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#content").offset().top
+	}, 1000, 'easeInOutExpo');
+});
+*/
+
+$('a[href^="#"]').on('click', function(event) {
+	var target = $(this).attr("href");
+	
+	if( target.length ) {
+		//event.preventDefault();
+		
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+		}, 1000);
+	}
+	
+	//console.log( $(target).offset().top );
+	console.log("click");
+});
+
+jQuery( window ).load(function() {
+	$(document).foundation();
+});
+
+jQuery( document ).ready(function() {	
+	fixScrollbar();
+	//fixHeights();
+	cover();
+});
+
+
+$( window ).resize(function() {
+	fixScrollbar();
+	//fixHeights();
+	cover();
+});	  
