d969402f6685bd3680c8fec202d6a294bd5a0f08
Fábián Gábor <dirtyred.photo@gmail.com>
2014-07-30 22:38:51 +0200

    optimization

--- js/vendor/scripts.js
+++ js/vendor/scripts.js
@@ -1,3 +1,5 @@
+var windowHeight = $( window ).height();
+
 function scrollbarWidth() {
 	//var div = $('<div style="width:50px;height:50px;overflow:hidden;position:absolute;top:-200px;left:-200px;"><div style="height:100px;"></div>');
 	var div = $('<div style="width:50px;height:50px;overflow:hidden;position:absolute;translate3d(-200px, -200px, 0);"><div style="height:100px;"></div>');
@@ -11,7 +13,7 @@ function scrollbarWidth() {
 }
 
 function fixHeights() {
-	var windowHeight = $( window ).height();
+	//var windowHeight = $( window ).height();
 	$( ".left-sidenav" ).height( windowHeight );
 	$( ".left-off-canvas-menu" ).height( windowHeight );	
 	$( ".single .placeholder" ).height( windowHeight );
@@ -20,30 +22,33 @@ function fixHeights() {
 }
 
 function fixScrollbar() {
-	var scale = $(window).width() / ( $(window).width()-scrollbarWidth() );
+	//var scale = $(window).width() / ( $(window).width()-scrollbarWidth() );
 	$('.archive .main-section').css( 'margin-right', -scrollbarWidth() );
 	$('.single .main-section').css( 'margin-right', -scrollbarWidth() );
 	
 	//$('.archive .main-section').css('transform', 'scaleX(' + scale + ')' ); // distorting!
 }
 
-function cover() {
-	var windowHeight = $( window ).height();
+function cover(windowHeight) {
+	//var windowHeight = $( window ).height();
 	//$("body.single-format-image #cover").height( windowHeight );	
 	var video = document.getElementById('video');
 	$("#video").css('transform', 'translate3d(0, ' + (windowHeight - $("#video").outerHeight())/2 + 'px, 0)' );
 	
-	//console.log( (windowHeight - $("#video").height())/2 );	
+	//console.log( (windowHeight - $("#video").height())/2 );
+	
+	console.log( windowHeight );
 }
 
-/*
+
 $("#scrollDown").click(function() {
 	$('html, body').animate({
 		scrollTop: $("#content").offset().top
-	}, 1000, 'easeInOutExpo');
+	//}, 1000, 'easeInOutExpo');
+	}, 1000);
 });
-*/
 
+/*
 $('a[href^="#"]').on('click', function(event) {
 	var target = $(this).attr("href");
 	
@@ -58,6 +63,11 @@ $('a[href^="#"]').on('click', function(event) {
 	//console.log( $(target).offset().top );
 	console.log("click");
 });
+*/
+
+$('html').on('scroll', function() {
+	//$('html, body').css('height', '100%');
+});
 
 jQuery( window ).load(function() {
 	$(document).foundation();
@@ -66,12 +76,18 @@ jQuery( window ).load(function() {
 jQuery( document ).ready(function() {	
 	fixScrollbar();
 	//fixHeights();
-	cover();
+	cover(windowHeight);
 });
 
-
+var resizeId;
 $( window ).resize(function() {
+	clearTimeout(resizeId);
+    resizeId = setTimeout(doneResizing, 250);
+});
+
+function doneResizing(){
+	var windowHeight = $( window ).height();
 	fixScrollbar();
 	//fixHeights();
-	cover();
-});	  
+	cover(windowHeight);
+}
